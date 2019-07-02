6651132400e8bf33c8a16ae2e872fa634b21db2d
Jesse Marple <jesse@ossium.co>
2019-07-02 17:33:08 -0400

    deploy files

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,4 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rundeck/docs-vuepress.git master:gh-pages
-
-cd -
+push-dir --dir=docs/.vuepress/dist --branch=gh-pages --cleanup
